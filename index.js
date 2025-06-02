967f033dd43c752021fe3abbca5f22032bf97445
Derek Walton <derekwalton19@gmail.com>
2025-06-02 10:58:02 +0100

    updated nav and icons

--- index.js
+++ index.js
@@ -12,3 +12,22 @@ for (const elem of arr) {
   })
   
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const faders = document.querySelectorAll('.fade-in-section');
+    const appearOptions = {
+        threshold: 0.15
+    };
+
+    const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) return;
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+});
